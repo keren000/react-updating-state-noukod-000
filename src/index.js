f6c88a5d4f068668f1fbf8406998a92bb8d7482a
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-01 16:50:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ClickityClick from './components/ClickityClick';
+<<<<<<< HEAD
 ReactDOM.render(<ClickityClick />,
 document.getElementById('root'));
+=======
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
+>>>>>>> 8da189e7d93dd3da1e801bf6b0a953043f57f12b
